9656f34384148ecdd35dea483f13beabc0aa2aee
Alex Hedges <git@alexhedges.dev>
2022-08-10 12:47:10 -0400

    Append Git hash to version string to work around pip issue
    This should not be merged upstream.

--- setup.py
+++ setup.py
@@ -1,6 +1,7 @@
 import sys
 import re
 import os.path
+import subprocess
 import zipfile
 
 from setuptools import setup
@@ -19,6 +20,16 @@ TASKS_JSON_FILE = "tasks.json"
 if not os.path.isfile(JAR_PATH):
     print('ERROR: Unable to find required library:', JAR_PATH)
     sys.exit(1)
+
+# Based on https://github.com/microsoft/DeepSpeed/blob/28dfca8a13313b570e1ad145cf14476d8d5d8e16/setup.py#L170-L184
+# Write out version/git info
+git_hash_cmd = "git rev-parse --short HEAD"
+try:
+    result = subprocess.check_output(git_hash_cmd, shell=True)
+    git_hash = result.decode('utf-8').strip()
+    VERSION += f'+{git_hash}'
+except subprocess.CalledProcessError:
+    pass
 
 with open(os.path.join('scienceworld', 'version.py'), 'w') as f:
     f.write(f'__version__ = {VERSION!r}\n')
